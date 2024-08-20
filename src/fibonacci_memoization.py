f13414082c8978645cb68b0832a8069a66ec7fe8
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2024-08-20 12:36:16 -0300

    feat: implementing fibonacci_memo result loop

--- src/fibonacci_memoization.py
+++ src/fibonacci_memoization.py
@@ -7,5 +7,14 @@ def fibonacci_memo(n, memo={}):
         result = 1
     else:
         result = fibonacci_memo(n-1, memo) + fibonacci_memo(n-2, memo)
-        memo[n] = result
-        return result
+    memo[n] = result
+    return result
+
+for i in range(8):
+    print()
+    if i == 0 or i == 1:
+        print(f"Para caso base para n = {i} retorna {i}")
+    else:
+        print(f"Entrada: fibonacci_memo({i}) calcula fibonacci_memo({i-1}) + fibonacci_memo({i-2}) que é {fibonacci_memo(i-1, memo={})} + {fibonacci_memo(i-2, memo={})}\nSaída: {fibonacci_memo(i, memo={})}")
+    print()
+    print("-"*100)
